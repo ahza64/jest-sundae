fc0340896166ec66f1cd7e1797874b64c90b3487
ahza64 <justinmichaelpettit@gmail.com>
2022-05-14 15:35:32 -0700

    update tests with overwritted test library render with a wrapper for the application context

--- src/pages/entry/test/Options.test.js
+++ src/pages/entry/test/Options.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../../test-utils/testing-library-utils'
 
 import Options from '../Options'
 import { OrderDetailsProvider } from '../../../context/OrderDetails'
@@ -14,7 +14,7 @@ test('displays image for each scoop option from the server', async () => {
 })
 
 test('Displays image for each topping option from the server', async () => {
-  render(<Options optionType='toppings' />, { wrapper: OrderDetailsProvider })
+  render(<Options optionType='toppings' />)
 
   const toppingImages = await screen.findAllByRole('img', { name: /topping$/i })
   expect(toppingImages).toHaveLength(3)
