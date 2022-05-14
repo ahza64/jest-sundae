7ac11cdcbdfabc09baa05ece90055a198b3e22f8
ahza64 <justinmichaelpettit@gmail.com>
2022-05-14 16:05:34 -0700

    update toppings total testing and add feature to app

--- src/pages/entry/test/totalUpdates.test.js
+++ src/pages/entry/test/totalUpdates.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../../test-utils/testing-library-utils'
 import userEvent from '@testing-library/user-event'
 import Options from '../Options'
 import { OrderDetailsProvider } from '../../../context/OrderDetails'
 
 test('update scoops total when scoops is updated', async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider })
+  render(<Options optionType="scoops" />)
   const scoopsSubtotal = screen.getByText( 'Scoops Total: $', { exact: false })
   expect(scoopsSubtotal).toHaveTextContent('0.00')
 
@@ -17,4 +17,21 @@ test('update scoops total when scoops is updated', async () => {
   userEvent.clear(chocolateInput)
   userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
+})
+
+test('update toppings total when toppings is updated', async () => {
+  render(<Options optionType="toppings" />)
+  const toppingsSubtotal = screen.getByText( 'Toppings Total: $', { exact: false })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', { name: 'Cherries' })
+  userEvent.click(cherriesCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', { name: 'Hot Fudge' })
+  userEvent.click(hotFudgeCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  userEvent.click(hotFudgeCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
 })
