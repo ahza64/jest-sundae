4bd5b2bc254f2bb2621563fa2fa667a164967172
ahza64 <justinmichaelpettit@gmail.com>
2022-05-17 11:15:57 -0700

    refactor/modularize the formatCurrency function to be more accessable

--- src/context/OrderDetails.js
+++ src/context/OrderDetails.js
@@ -1,13 +1,6 @@
 import { useState, useContext, createContext, useMemo, useEffect } from 'react'
 import { pricePerItem } from '../constants'
-
-function formatCurrency(amount) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  }).format(amount)
-}
+import { formatCurrency } from '../utilities'
 
 const OrderDetails = createContext()
 
---
+++ src/utilities/index.js
@@ -0,0 +1,8 @@
+// format a float number to a money string
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  }).format(amount)
+}
