d0b030732645d26a25821739df090078c7519097
ahza64 <justinmichaelpettit@gmail.com>
2022-05-28 12:35:21 -0700

    server update

--- jest-sundae-server/server.js
+++ jest-sundae-server/server.js
@@ -40,7 +40,7 @@ app.post('/order', (req, res) => {
 })
 
 if (require.main === module) {
-  app.listen(3030, () => console.log('Sundae server listening on port 3030!'))
+  app.listen(process.env.PORT || 3030, () => console.log('Sundae server listening on port 3030!'))
 }
 
 module.exports = app;
